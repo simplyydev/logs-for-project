d960aec4a73f812cba5d90d91ff4dca48e6e6d02
Devendra <devendranath2004@gmail.com>
2026-04-02 10:49:09 +0530

    fix: complete supabase middleware typing

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -4,20 +4,28 @@ import { cookies } from 'next/headers';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
 
-export const createClient = async () => {
-  const cookieStore = await cookies();
+export const createClient = () => {
+  const cookieStore = cookies();
+
   return createServerClient(supabaseUrl, supabaseKey, {
     cookies: {
       getAll() {
         return cookieStore.getAll();
       },
-      setAll(cookiesToSet) {
+
+      setAll(cookiesToSet: {
+        name: string;
+        value: string;
+        options?: Record<string, any>;
+      }[]) {
         try {
-          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options)
+          );
         } catch {
-          // ignored in server component contexts
+          // ignore in server components
         }
-      }
-    }
+      },
+    },
   });
 };
